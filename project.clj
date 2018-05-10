887cb6d000e76c98820b2c9d19d4a01f2b57b1df
Milt Reder <milt@yetanalytics.com>
2018-05-10 13:46:13 -0400

    alpha15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.yetanalytics/xapi-schema "1.0.0-alpha14"
+(defproject com.yetanalytics/xapi-schema "1.0.0-alpha15"
   :description "Clojure(script) Schema for the Experience API v1.0.3"
   :url "https://github.com/yetanalytics/xapi-schema"
   :license {:name "Eclipse Public License"
